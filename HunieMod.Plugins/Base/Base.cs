184459adef9fe0d4daa9a5c50c461ab9d1b9c0b9
terrance1996 <johndevis112@outlook.com>
2020-05-20 07:26:23 +0200

    Add Base plugin

---
+++ HunieMod.Plugins/Base/Base.cs
@@ -0,0 +1,9 @@
+﻿using BepInEx;
+
+namespace HunieMod.Plugins.Base
+{
+    [BepInPlugin("org.lounger.huniemod.Base", "Base", "1.0.0.0")]
+    public class Base : BaseUnityPlugin
+    {
+    }
+}
---
+++ HunieMod.Plugins/Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HunieMod.Plugins.Base")]
+[assembly: AssemblyDescription("HunieMod plugin providing several abstractions and helpers as a base for other plugins")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lounger")]
+[assembly: AssemblyProduct("HunieMod.Plugins.Base")]
+[assembly: AssemblyCopyright("Copyright © Lounger 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("827c84eb-7dd9-4c0d-b93e-1786ebd8adf4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
