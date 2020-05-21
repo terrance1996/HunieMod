265acba3b97c261dd73870ed717167b0b921210a
terrance1996 <johndevis112@outlook.com>
2020-05-21 22:59:59 +0200

    Rename Base to HunieModPlugin and add docs

--- HunieMod.Plugins/Base/Base.cs
+++
@@ -1,9 +0,0 @@
-﻿using BepInEx;
-
-namespace HunieMod.Plugins.Base
-{
-    [BepInPlugin("org.lounger.huniemod.Base", "Base", "1.0.0.0")]
-    public class Base : BaseUnityPlugin
-    {
-    }
-}
---
+++ HunieMod.Plugins/Base/HunieModPlugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace HunieMod.Plugins.Base
+{
+    /// <summary>
+    /// The base plugin type that adds HunieMod specific functionality over the default BepInEx plugin loader
+    /// </summary>
+    [BepInPlugin("org.lounger.huniemod", "HunieMod Base", "1.0.0.0")]
+    public class HunieModPlugin : BaseUnityPlugin
+    {
+    }
+}
