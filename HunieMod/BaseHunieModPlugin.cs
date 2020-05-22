76474a933faa73dcc5525955fbdcca82a522e2ad
terrance1996 <johndevis112@outlook.com>
2020-05-22 06:17:47 +0200

    Remove HunieModPlugin attribute
    Extending BepInPlugin prevents Chainloader to find the plugin's metadata

--- HunieMod/BaseHunieModPlugin.cs
+++ HunieMod/BaseHunieModPlugin.cs
@@ -5,7 +5,7 @@ namespace HunieMod
     /// <summary>
     /// The base plugin type that adds HunieMod specific functionality over the default <see cref="BaseUnityPlugin"/>
     /// </summary>
-    [HunieModPlugin("org.lounger.huniemod", "HunieMod Base", "1.0.0.0", "Lounger")]
+    [BepInPlugin("org.lounger.huniemod", "HunieMod Base", "1.0.0.0")]
     public class BaseHunieModPlugin : BaseUnityPlugin
     {
     }
