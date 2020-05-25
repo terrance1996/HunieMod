015de9cc584fd6a222d3434db222f6a835d4bf93
terrance1996 <johndevis112@outlook.com>
2020-05-25 13:14:09 +0200

    Add GirlId enum

---
+++ HunieMod/GirlId.cs
@@ -0,0 +1,68 @@
+﻿namespace HunieMod.Plugins.Base
+{
+    /// <summary>
+    /// Girl IDs that can be compared to the <see cref="Definition.id"/> field on <see cref="GirlDefinition"/>
+    /// </summary>
+    public enum GirlId
+    {
+        /// <summary>
+        /// Tiffany Maye
+        /// </summary>
+        Tiffany = 1,
+
+        /// <summary>
+        /// Aiko Yumi
+        /// </summary>
+        Aiko,
+
+        /// <summary>
+        /// Kyanna Delrio
+        /// </summary>
+        Kyanna,
+
+        /// <summary>
+        /// Audrey Belrose
+        /// </summary>
+        Audrey,
+
+        /// <summary>
+        /// Lola Rembrite
+        /// </summary>
+        Lola,
+
+        /// <summary>
+        /// Nikki Ann-Marie
+        /// </summary>
+        Nikki,
+
+        /// <summary>
+        /// Jessie Maye
+        /// </summary>
+        Jessie,
+
+        /// <summary>
+        /// Beli Lapran
+        /// </summary>
+        Beli,
+
+        /// <summary>
+        /// Kyu Sugardust
+        /// </summary>
+        Kyu,
+
+        /// <summary>
+        /// Momo
+        /// </summary>
+        Momo,
+
+        /// <summary>
+        /// Celeste Luvendass
+        /// </summary>
+        Celeste,
+
+        /// <summary>
+        /// Theiatena Venus
+        /// </summary>
+        Venus
+    }
+}
