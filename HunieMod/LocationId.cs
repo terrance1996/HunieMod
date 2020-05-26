12c19319f2b580ec5bc65006a9c0b935ff7bed02
terrance1996 <johndevis112@outlook.com>
2020-05-26 06:57:04 +0200

    Add LocationId enum

---
+++ HunieMod/LocationId.cs
@@ -0,0 +1,118 @@
+﻿namespace HunieMod
+{
+    /// <summary>
+    /// Girl IDs that can be compared to the <see cref="Definition.id"/> field on <see cref="LocationDefinition"/>
+    /// </summary>
+    public enum LocationId
+    {
+        /// <summary>
+        /// Your Bedroom (Date - Indoor - Bonus)
+        /// </summary>
+        BedroomBonus = 1,
+
+        /// <summary>
+        /// Dawnwood Park (Normal - Outdoor)
+        /// </summary>
+        Park = 2,
+
+        /// <summary>
+        /// Nutmeg Cafe (Normal - Indoor)
+        /// </summary>
+        Cafe = 3,
+
+        /// <summary>
+        /// Turtle Bay Beach (Normal - Outdoor)
+        /// </summary>
+        Beach = 4,
+
+        /// <summary>
+        /// Bar & Lounge (Normal - Indoor)
+        /// </summary>
+        Bar = 5,
+
+        /// <summary>
+        /// Corkscrew Carnival (Date - Indoor)
+        /// </summary>
+        Carnival = 6,
+
+        /// <summary>
+        /// University Campus (Normal - Outdoor)
+        /// </summary>
+        Campus = 7,
+
+        /// <summary>
+        /// Fitness Club (Normal - Indoor)
+        /// </summary>
+        Gym = 8,
+
+        /// <summary>
+        /// Shopping Mall (Normal - Indoor)
+        /// </summary>
+        Mall = 9,
+
+        /// <summary>
+        /// Farmers Market (Date - Indoor)
+        /// </summary>
+        Market = 10,
+
+        /// <summary>
+        /// Lusties Nightclub (Normal - Indoor)
+        /// </summary>
+        Nightclub = 11,
+
+        /// <summary>
+        /// Gold Falls Casino (Date - Indoor)
+        /// </summary>
+        Casino = 12,
+
+        /// <summary>
+        /// Outdoor Lounge (Date - Indoor)
+        /// </summary>
+        Lounge = 13,
+
+        /// <summary>
+        /// Botanical Garden (Date - Indoor)
+        /// </summary>
+        Garden = 14,
+
+        /// <summary>
+        /// Hot Spring (Date - Indoor)
+        /// </summary>
+        Spring = 15,
+
+        /// <summary>
+        /// Hiking Trail (Date - Indoor)
+        /// </summary>
+        Trail = 16,
+
+        /// <summary>
+        /// Ice Rink (Date - Indoor)
+        /// </summary>
+        Rink = 17,
+
+        /// <summary>
+        /// Water Park (Date - Indoor)
+        /// </summary>
+        WaterPark = 18,
+
+        /// <summary>
+        /// Tennis Courts (Date - Indoor)
+        /// </summary>
+        Courts = 19,
+
+        /// <summary>
+        /// Scenic Overlook (Date - Indoor)
+        /// </summary>
+        Overlook = 20,
+
+        /// <summary>
+        /// Vinnie's Restaurant (Date - Indoor)
+        /// </summary>
+        Restaurant = 21,
+
+        /// <summary>
+        /// Your Bedroom (Normal - Indoor - After bonus)
+        /// </summary>
+        Bedroom = 22
+    }
+}
