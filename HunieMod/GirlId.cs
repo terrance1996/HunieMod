a92b1b07b31bb30b65b4c60767f5952546c53be1
terrance1996 <johndevis112@outlook.com>
2020-06-04 17:35:43 +0200

    Remove newlines on enums

--- HunieMod/GirlId.cs
+++ HunieMod/GirlId.cs
@@ -9,57 +9,46 @@
         /// Tiffany Maye
         /// </summary>
         Tiffany = 1,
-
         /// <summary>
         /// Aiko Yumi
         /// </summary>
         Aiko,
-
         /// <summary>
         /// Kyanna Delrio
         /// </summary>
         Kyanna,
-
         /// <summary>
         /// Audrey Belrose
         /// </summary>
         Audrey,
-
         /// <summary>
         /// Lola Rembrite
         /// </summary>
         Lola,
-
         /// <summary>
         /// Nikki Ann-Marie
         /// </summary>
         Nikki,
-
         /// <summary>
         /// Jessie Maye
         /// </summary>
         Jessie,
-
         /// <summary>
         /// Beli Lapran
         /// </summary>
         Beli,
-
         /// <summary>
         /// Kyu Sugardust
         /// </summary>
         Kyu,
-
         /// <summary>
         /// Momo
         /// </summary>
         Momo,
-
         /// <summary>
         /// Celeste Luvendass
         /// </summary>
         Celeste,
-
         /// <summary>
         /// Theiatena Venus
         /// </summary>
